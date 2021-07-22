da40d6f4e5f150a69efe706193e9908677537d5f
Arturs Olekss <arturs.olekss@gmail.com>
2021-07-22 20:11:53 +0300

    22_07_2021 (part 2)

--- Arrays/Arrays1.cs
+++ Arrays/Arrays1.cs
@@ -11,14 +11,36 @@ namespace Arrays
             // Console.WriteLine(arr[1]);
             // Console.WriteLine(arr[0]);
             // Console.WriteLine(arr[3]);
+
             string[] arr = new string[5];
-            AddNewElement(0, ref arr);
+            int elemtnPosi = 0;
+            AddNewElement(elemtnPosi, ref arr);
             AddNewElement(1, ref arr);
             AddNewElement(2, ref arr);
             AddNewElement(3, ref arr);
             AddNewElement(4, ref arr);
+            // AddNewElement(5, ref arr);CAN'T DO THIS, BECAUSE THERE ARE 5 CELLS ONLY!
 
             Console.WriteLine(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+            Console.WriteLine("The lenght of the array is " + arr.Length);
+
+            //2-Dimensional arrays
+
+            //   string[][] arr2Dim = new string[][];///this is not correct
+            // string[][] arr2Dim = new string[3][];///this is correct
+
+            // arr2Dim[0] = new string[3];
+            // arr2Dim[1] = new string[2];
+            // arr2Dim[2] = new string[4];
+
+            // arr2Dim[0][2] = "Here is the element";
+            // arr2Dim[2][3] = "Here is another element";
+
+            // string[,] arr2Dim = new string[3, 3];///this is correct
+
+            // arr2Dim[0, 2] = "Here is the element";
+            // arr2Dim[2, 2] = "Here is another element";
+
         }
 
         public static void AddNewElement(int elementIndex, ref string[] arr)
---
+++ Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the numbers from 1 to 10
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
