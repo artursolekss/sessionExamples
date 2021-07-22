74b68e22bca433801085422537ce16faf34a61d6
Arturs Olekss <arturs.olekss@gmail.com>
2021-07-22 21:30:15 +0300

    22_07_2021_final

--- Arrays/Arrays1.cs
+++ Arrays/Arrays1.cs
@@ -12,16 +12,35 @@ namespace Arrays
             // Console.WriteLine(arr[0]);
             // Console.WriteLine(arr[3]);
 
-            string[] arr = new string[5];
-            int elemtnPosi = 0;
-            AddNewElement(elemtnPosi, ref arr);
-            AddNewElement(1, ref arr);
-            AddNewElement(2, ref arr);
-            AddNewElement(3, ref arr);
-            AddNewElement(4, ref arr);
+            // string[] arr = new string[5];
+            // int elemtnPosi = 0;
+            // AddNewElement(elemtnPosi, ref arr);
+            // AddNewElement(1, ref arr);
+            // AddNewElement(2, ref arr);
+            // AddNewElement(3, ref arr);
+            // AddNewElement(4, ref arr);
             // AddNewElement(5, ref arr);CAN'T DO THIS, BECAUSE THERE ARE 5 CELLS ONLY!
 
-            Console.WriteLine(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+            // Console.WriteLine(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]);
+
+            Console.WriteLine("How many elements of the array there are?");
+            int numberOfElements;
+            if (!Int32.TryParse(Console.ReadLine(), out numberOfElements))
+            {
+                Console.WriteLine("Wrong input!");
+                return;
+            }
+
+            string[] arr = new string[numberOfElements];
+            for (int i = 0; i < arr.Length; i++)//at this point arr.Length is the same as numberOfElements
+                AddNewElement(i, ref arr);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.Write("\n");
+
             Console.WriteLine("The lenght of the array is " + arr.Length);
 
             //2-Dimensional arrays
--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,10 +6,40 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            //Print the numbers from 1 to 10
-            for (int i = 1; i < 11; i++)
+            // //Print the numbers from 1 to 10
+            // for (int i = -10; i < 0; i--)
+            // {
+            //     // for (;true; )
+            //     // {
+            //     //     Console.WriteLine("Loop");
+            //     Console.WriteLine(i);
+            //     if (i == -20)
+            //         break;//staement can be used to break execution the loop
+            // }
+
+            // for (int i = 1; i < 20; i *= 2)
+            // {
+            //     Console.WriteLine("The current iteraton is : " + i);
+            // }
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     for (int j = 0; j < 10 - i; j++)
+            //     {
+            //         Console.Write("*");
+            //     }
+            //     Console.Write("\n");
+            // }
+
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                for (int j = 0; j < 20; j++)
+                {
+                    if (j > 8 - i && j < 11 + i)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
             }
         }
     }
