07f1e5f751506dc7d1f425682900b17cabc2d92c
Arturs Olekss <arturs.olekss@gmail.com>
2021-09-09 20:23:08 +0300

    09_09_2021

---
+++ Generics/GenericsDemo/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    class Calculator<T1,T2> where T1:IComparable where T2:IComparable
+    {
+
+      public bool checkGreater(T1 a, T1 b)
+        {
+            if (a.CompareTo(b) > 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool checkLess(T2 a, T2 b)
+        {
+            if (a.CompareTo(b) < 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
---
+++ Generics/GenericsDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<int,string> cal = new Calculator<int,string>();
+            Console.WriteLine(cal.checkGreater(10, 4));
+            Console.WriteLine(cal.checkLess("AA", "BB"));
+
+            Calculator<string,int> calStr = new Calculator<string,int>();
+            Console.WriteLine(calStr.checkGreater("AAA", "BBB"));
+            Console.WriteLine(calStr.checkLess(10, 4));
+
+            LinkedList<string> myStrList = new LinkedList<string>();
+
+               myStrList.AddLast("Str1");
+            myStrList.AddLast("Str2");
+
+            //myStrList.AddLast(423432);
+
+        }
+    }
+}
